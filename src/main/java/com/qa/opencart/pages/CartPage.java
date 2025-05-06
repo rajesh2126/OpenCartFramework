73c2c0e1713b9de3d662feaf0496d652e79cb496
Naveen AutomationLabs <naveenanimation20@gmail.com>
2025-05-06 09:27:06 +0530

    changes are done as per NaveenTL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,10 +1,12 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
-	int i = 10;
+	private By cart = By.id("cart");
 	
 	public void cart() {
-		System.out.println("cart method");
+		System.out.println("cart using"+ cart);
 	}
 }
