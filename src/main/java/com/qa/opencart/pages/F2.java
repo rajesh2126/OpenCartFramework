fbd404ae4b73fb0b1863267053bcae9d8693d03f
Naveen AutomationLabs <naveenanimation20@gmail.com>
2025-05-07 09:19:55 +0530

    f1 feature

---
+++ src/main/java/com/qa/opencart/pages/F2.java
@@ -0,0 +1,9 @@
+package com.qa.opencart.pages;
+
+public class F2 {
+
+	public static void main(String[] args) {
+		System.out.println("f2 feature");
+	}
+
+}
---
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		System.out.println("naveen testing code");
+		
+	}
+
+}
