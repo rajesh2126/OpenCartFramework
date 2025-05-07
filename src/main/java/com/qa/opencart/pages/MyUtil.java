cb8d51e2082bad586d3aa1b9b4943dd2a73460e6
Naveen AutomationLabs <naveenanimation20@gmail.com>
2025-05-07 09:56:09 +0530

    added login method features and resolved the conflicts after discussion with Richa

--- src/main/java/com/qa/opencart/pages/MyUtil.java
+++ src/main/java/com/qa/opencart/pages/MyUtil.java
@@ -3,7 +3,9 @@ package com.qa.opencart.pages;
 public class MyUtil {
 	
 	public void login() {
-		System.out.println("richa - login");
+
+		System.out.println("naveen - login");
+		System.out.println("login is done..");
 	}
 	
 	
