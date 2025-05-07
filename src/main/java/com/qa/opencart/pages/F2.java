60f13ccd2154207949deb0de67b88a236ce6db5f
Naveen AutomationLabs <naveenanimation20@gmail.com>
2025-05-07 09:32:40 +0530

    deleted files

--- src/main/java/com/qa/opencart/pages/F2.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class F2 {
-
-	public static void main(String[] args) {
-		System.out.println("f2 feature");
-	}
-
-}
--- src/main/java/com/qa/opencart/pages/Rest.java
+++
@@ -1,9 +0,0 @@
-package com.qa.opencart.pages;
-
-public class Rest {
-
-	public static void main(String[] args) {
-		System.out.println("Richa Rest code");
-	}
-
-}
